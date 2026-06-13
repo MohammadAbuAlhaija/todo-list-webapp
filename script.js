ce10a4cd64cd744d5f51bef6c56bcc0545f63873
Mohammad Abu Alhaija <eng.mohammadabualhija@gmail.com>
2026-06-13 10:08:40 +0300

    Add task input functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
+addBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
